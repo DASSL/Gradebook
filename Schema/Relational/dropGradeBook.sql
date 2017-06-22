d3fdc96af471bb0b2fa80830569e0d961b18ade0
Steven Rollo <srrollo@gmail.com>
2017-06-22 13:47:17 -0400

    Updated copyright info

--- Schema/Relational/dropGradeBook.sql
+++ Schema/Relational/dropGradeBook.sql
@@ -1,7 +1,12 @@
---Data Science & Systems Lab at Western Connecticut State University (dassl@WCSU)
---(C) 2017 DASSL CC 4.0 BY-SA-NC https://creativecommons.org/licenses/by-nc-sa/4.0/
---
 --dropGradeBook.sql - GradeBook
+
+--Zaid Bhujwala, Zach Boylan, Steven Rollo, Sean Murthy
+--Data Science & Systems Lab (DASSL), Western Connecticut State University (WCSU)
+
+--(C) 2017- DASSL. ALL RIGHTS RESERVED.
+--Licensed to others under CC 4.0 BY-SA-NC: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+--PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
 
 DROP TABLE IF EXISTS Submission;
 DROP TABLE IF EXISTS Enrollee;
