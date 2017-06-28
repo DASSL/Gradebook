b58ab13c0d31dbd52c861adbc442b179548667fa
Steven Rollo <srrollo@gmail.com>
2017-06-28 14:48:19 -0400

    Added openCloseImport schema:
    Added to populateFromOpenClose.sql

--- src/populateFromOpenClose.sql
+++ src/populateFromOpenClose.sql
@@ -9,6 +9,31 @@
 --PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
 
 --A function to return individual rows when given a list of instructors that include csv lists
+DROP TABLE IF EXISTS openCloseImport;
+
+CREATE TABLE openCloseImport
+(
+   Status VARCHAR(6),
+   "Level" VARCHAR(2),
+   CRN  VARCHAR(5),
+   Subject VARCHAR(4),
+   Course VARCHAR(6), 
+   "Section" VARCHAR(3),
+   Credits VARCHAR(15),
+   Title VARCHAR(100),
+   Days VARCHAR(7),
+   "Time" VARCHAR(30),
+   "Date" VARCHAR(15),
+   Capacity INTEGER,
+   Actual INTEGER,
+   Remaining INTEGER,
+   XL_Capacity INTEGER,
+   XL_Actual INTEGER,
+   XL_Remaining INTEGER,
+   Location VARCHAR(25),
+   Instructor VARCHAR(200)
+);
+
 CREATE OR REPLACE FUNCTION instructorUnnest("instructorNames" VARCHAR(200))
 RETURNS TABLE("Name" VARCHAR(100)) AS
 $$
