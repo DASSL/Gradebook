42723c8cb9c2e610bcb4bdc011082ca9b4789e9b
Andrew Figueroa <figueroa039@connect.wcsu.edu>
2017-06-29 13:47:38 -0400

    Merge rosterStaging into studentImport
    Combine all SQL code into one file to reduce complexity. The user now
    only needs to run one script file. After that they can simply run the
    copy command and the importStudent function. (This current file and
    function may be renamed)

--- src/studentImport.sql
+++ src/studentImport.sql
@@ -10,6 +10,28 @@
 --PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
 
 
+CREATE TABLE IF NOT EXISTS rosterStaging
+(
+   LName VARCHAR(32),
+   FName VARCHAR(32),
+   MName VARCHAR(32),
+   ID VARCHAR(32),
+   RegStatus VARCHAR(50),
+   Level VARCHAR(30),
+   Degree VARCHAR(50),
+   Program VARCHAR(50),
+   Major VARCHAR(50),
+   Class VARCHAR(25),
+   Credits INTEGER,
+   Email VARCHAR(100)
+);
+
+TRUNCATE rosterStaging;
+
+--psql command:
+--\COPY rosterStaging FROM <filename> WITH csv HEADER
+
+
 --This function imports students that are currently in the rosterStaging folder.
 -- The sectionID corresponds to a section in the Section table from the Gradebook
 -- schema, which is determined by Term, Course, and SectionNumber
@@ -17,21 +39,21 @@
 CREATE OR REPLACE FUNCTION importStudents(Term INTEGER, Course VARCHAR(8), 
    SectionNumber VARCHAR(3), enrollmentDate DATE DEFAULT current_date) RETURNS VOID AS
 $$
-   INSERT INTO Student(FName, MName, LName, SchoolIssuedID, Email, Major, Year)
+   INSERT INTO public.Student(FName, MName, LName, SchoolIssuedID, Email, Major, Year)
    SELECT r.FName, r.MName, r.MName, r.ID, r.email, r.Major, r.class
    FROM rosterStaging r
    ON CONFLICT (SchoolIssuedID) DO UPDATE FName = EXCLUDED.FName, MName = 
          EXCLUDED.MName, LName = EXCLUDED.LName, Email = EXCLUDED.email, 
 		 Major = EXCLUDED.Major, Year = EXCLUDED.Year;
    
-   INSERT INTO Enrollee(Student, Section, DateEnrolled, YearEnrolled, MajorEnrolled)
+   INSERT INTO public.Enrollee(Student, Section, DateEnrolled, YearEnrolled, MajorEnrolled)
    WITH sectionID AS (
       SELECT ID
 	  FROM public.Section S
 	  WHERE Term = S.Term AND Course = S.course AND SectionNumber = S.sectionNumber
    )
    SELECT Stu.ID, sectionID.ID, $4, r.class, r.Major
-   FROM rosterStaging r JOIN Student Stu ON r.ID = Stu.schoolIssuedID,
+   FROM rosterStaging r JOIN public.Student Stu ON r.ID = Stu.schoolIssuedID,
         sectionID;
 $$ LANGUAGE SQL
    SET search_path TO '$user, public';
