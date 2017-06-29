5073cea3c902c499b433a24aee0e483126788170
Steven Rollo <srrollo@gmail.com>
2017-06-29 14:27:07 -0400

    Fixed batch file quoting

--- src/importOpenCloseCSV.bat
+++ src/importOpenCloseCSV.bat
@@ -1,6 +1,6 @@
+@ECHO OFF
 REM Batch file to import openclose Data
 REM USAGE: importOpenCloseCSV.bat host port database username "filename" year 'term'
 
-@ECHO OFF
-psql -h %1 -p %2 -d %3 -U %4 -c "TRUNCATE openCloseStaging;" -c "\COPY openCloseStaging FROM %5 WITH csv HEADER" -c "SELECT openCloseImport(%6, %7);"
+psql -h %1 -p %2 -d %3 -U %4 -c "TRUNCATE openCloseStaging;" -c "\COPY openCloseStaging FROM %5 WITH csv HEADER" -c "SELECT openCloseImport(%6, '%7');"
 pause
