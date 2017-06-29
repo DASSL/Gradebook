f91540cf77047dc64f0169fca89deec661ffb9d0
Kyle Bella <bella004@connect.wcsu.edu>
2017-06-29 13:29:13 -0400

    Updated queries for better idempotency.
    Updates ON CONFLICT clause on Student table and inserts into more columns on the Enrollee table.

--- src/studentImport.sql
+++ src/studentImport.sql
@@ -37,18 +37,18 @@ CREATE OR REPLACE FUNCTION importStudents(Term INTEGER, Course VARCHAR(8),
    SectionNumber VARCHAR(3), enrollmentDate DATE DEFAULT current_date) RETURNS VOID AS
 $$
    INSERT INTO Student(FName, MName, LName, SchoolIssuedID, Email, Major, Year)
-   SELECT substring(I.FName FOR 30), substring(I.MName FOR 30), substring(I.MName FOR 30),
-         substring(I.ID FOR 30), I.email, I.Major, I.class
+   SELECT I.FName, I.MName, I.MName, I.ID, I.email, I.Major, I.class
    FROM rosterImport I
-   ON CONFLICT (SchoolIssuedID) DO NOTHING;
+   ON CONFLICT (SchoolIssuedID) DO UPDATE FName = EXCLUDED.FName, MName = EXCLUDED.MName, LName = EXCLUDED.LName,
+         Email = EXCLUDED.email, Major = EXCLUDED.Major, Year = EXCLUDED.class;
    
-   INSERT INTO Enrollee(Student, Section, EnrollmentDate)
+   INSERT INTO Enrollee(Student, Section, DateEnrolled, YearEnrolled, MajorEnrolled)
    WITH sectionID AS (
       SELECT ID
 	  FROM Section S
 	  WHERE Term = S.Term AND Course = S.course AND SectionNumber = S.sectionNumber
    )
-   SELECT Stu.ID, sectionID.ID, $4
+   SELECT Stu.ID, sectionID.ID, $4, r.class, r.Major
    FROM rosterImport r JOIN Student Stu ON substring(r.ID FOR 30) = Stu.schoolIssuedID,
         sectionID;
 $$ LANGUAGE SQL;
