3e53727c3bb08e05e99ce1ae16272555a5aee192
Andrew Figueroa <figueroa039@connect.wcsu.edu>
2017-06-29 13:41:34 -0400

    Make updates to importStudents function
    - Wrap to ~80 character per line
    - Remove unnecessary comment
    - Update name of staging table

--- src/studentImport.sql
+++ src/studentImport.sql
@@ -10,31 +10,28 @@
 --PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
 
 
---During the INSERTs into the Student table, FName, MName, LName, and ID are 
--- being truncated from the staging table due to MD5 hashes being 32 characters long
-
---This function imports students that are currently in the rosterImport folder.
+--This function imports students that are currently in the rosterStaging folder.
 -- The sectionID corresponds to a section in the Section table from the Gradebook
--- schema, which is determined by Term, Course, and sectionNumber
+-- schema, which is determined by Term, Course, and SectionNumber
 
 CREATE OR REPLACE FUNCTION importStudents(Term INTEGER, Course VARCHAR(8), 
    SectionNumber VARCHAR(3), enrollmentDate DATE DEFAULT current_date) RETURNS VOID AS
 $$
    INSERT INTO Student(FName, MName, LName, SchoolIssuedID, Email, Major, Year)
-   SELECT I.FName, I.MName, I.MName, I.ID, I.email, I.Major, I.class
-   FROM rosterImport I
-   ON CONFLICT (SchoolIssuedID) DO UPDATE FName = EXCLUDED.FName, MName = EXCLUDED.MName, LName = EXCLUDED.LName,
-         Email = EXCLUDED.email, Major = EXCLUDED.Major, Year = EXCLUDED.class;
+   SELECT r.FName, r.MName, r.MName, r.ID, r.email, r.Major, r.class
+   FROM rosterStaging r
+   ON CONFLICT (SchoolIssuedID) DO UPDATE FName = EXCLUDED.FName, MName = 
+         EXCLUDED.MName, LName = EXCLUDED.LName, Email = EXCLUDED.email, 
+		 Major = EXCLUDED.Major, Year = EXCLUDED.Year;
    
    INSERT INTO Enrollee(Student, Section, DateEnrolled, YearEnrolled, MajorEnrolled)
-
    WITH sectionID AS (
       SELECT ID
 	  FROM public.Section S
 	  WHERE Term = S.Term AND Course = S.course AND SectionNumber = S.sectionNumber
    )
    SELECT Stu.ID, sectionID.ID, $4, r.class, r.Major
-   FROM rosterImport r JOIN Student Stu ON substring(r.ID FOR 30) = Stu.schoolIssuedID,
+   FROM rosterStaging r JOIN Student Stu ON r.ID = Stu.schoolIssuedID,
         sectionID;
 $$ LANGUAGE SQL
    SET search_path TO '$user, public';
