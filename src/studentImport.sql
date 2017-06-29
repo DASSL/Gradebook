a28a74a364c79223e4b69de2fd71a367eb18c435
Andrew Figueroa <figueroa039@connect.wcsu.edu>
2017-06-29 12:33:44 -0400

    Remove SectionID variant of importStudents
    This variant was originally created for testing the major behavior of
    the function. The other importStudent function takes Term(id), Course,
    and Section to import the roster.

--- src/studentImport.sql
+++ src/studentImport.sql
@@ -16,23 +16,6 @@
 --This function imports students that are currently in the rosterImport folder.
 -- The sectionID corresponds to a section in the Section table from the Gradebook schema
 
-CREATE OR REPLACE FUNCTION importStudents(SectionID INTEGER, enrollmentDate DATE 
-   DEFAULT current_date) RETURNS VOID AS
-$$
-   INSERT INTO Student(FName, MName, LName, SchoolIssuedID, Email, Major, Year)
-   SELECT substring(I.FName FOR 30), substring(I.MName FOR 30), substring(I.MName FOR 30),
-         substring(I.ID FOR 30), I.email, I.Major, I.class
-   FROM rosterImport I
-   ON CONFLICT (SchoolIssuedID) DO NOTHING; --Ignore repeated students
-
-   INSERT INTO Enrollee(Student, Section, EnrollmentDate)
-   SELECT Stu.ID, $1, $2
-   FROM rosterImport r JOIN Student Stu ON substring(r.ID FOR 30) = Stu.schoolIssuedID;
-$$ LANGUAGE SQL;
-
-
---This function has the same behavior as the previous function, but uses Term, Course, and SectionNumber as an alternative to SectionID
-
 CREATE OR REPLACE FUNCTION importStudents(Term INTEGER, Course VARCHAR(8), 
    SectionNumber VARCHAR(3), enrollmentDate DATE DEFAULT current_date) RETURNS VOID AS
 $$
