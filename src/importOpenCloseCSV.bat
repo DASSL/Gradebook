4957609d97fb4718bee03fa22e628760096c0f30
Steven Rollo <srrollo@gmail.com>
2017-06-29 21:15:32 -0400

    Updated batch file for import openclose
    Batch file now takes filename, year and season
    as mandatory parameters.  username, database, and
    server:port are all optional

--- src/importOpenCloseCSV.bat
+++ src/importOpenCloseCSV.bat
@@ -1,6 +1,42 @@
 @ECHO OFF
 REM Batch file to import openclose Data
-REM USAGE: importOpenCloseCSV.bat host port database username "filename" year 'term'
+REM USAGE: importOpenCloseCSV.bat "filename" year season username database server:port
 
-psql -h %1 -p %2 -d %3 -U %4 -c "TRUNCATE openCloseStaging;" -c "\COPY openCloseStaging FROM %5 WITH csv HEADER" -c "SELECT openCloseImport(%6, '%7');"
+IF "%1"=="" GOTO usage
+
+IF "%3"=="" GOTO argError
+
+IF "%4"=="" (
+    SET username=postgres
+) ELSE (
+    SET username=%4
+)
+
+IF "%5"=="" (
+    SET database=postgres
+) ELSE (
+    SET database=%5
+)
+
+IF "%6"=="" (
+   SET hostname=localhost
+) ELSE (
+   FOR /f "tokens=1,2 delims=:" %%a in ("%6") DO SET hostname=%%a&SET port=%%b
+)
+
+IF "%port%"=="" SET port=5432
+
+psql -h %hostname% -p %port% -d %database% -U %username% -c "TRUNCATE openCloseStaging;" -c "\COPY openCloseStaging FROM %1 WITH csv HEADER" -c "SELECT openCloseImport(%2, '%3');"
+goto end
+
+:argError
+ECHO You must supply at least three arguments (filename year season)
+
+:usage
+ECHO importOpenCloseCSV.bat: Imports a CSV from OpenClose into Gradebook
+ECHO Takes 3-6 space separated arguments
+ECHO Usage:
+ECHO importOpenCloseCSV.bat "filename" year season username database server:port
+
+:end
 pause
