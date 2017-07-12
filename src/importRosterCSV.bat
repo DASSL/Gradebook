b9ea5fa28a585df489f73d64f6dc73a7391750b7
Andrew Figueroa <figueroa039@connect.wcsu.edu>
2017-07-12 09:33:07 -0400

    Improve comments in importRosterCSV.bat
    Add spacing, move optional arguments comment to a better location, add
    comments to explain the single-transaction flag.

--- src/importRosterCSV.bat
+++ src/importRosterCSV.bat
@@ -10,18 +10,19 @@ REM Copyright (c) 2017- DASSL. ALL RIGHTS RESERVED.
 
 REM ALL ARTIFACTS PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
 
-REM Batch file to importFromRoster data
-REM Up to 9 additional parameters can be provided following the required ones
 
+REM Batch file to importFromRoster data
 REM USAGE: importRosterCSV.bat "filename" year season 'courseNumber' 'sectionNumber' username database server:port optional-psql-commands
 REM year like 2017
 REM season is the "order" attribute of the Season table
 REM courseNumber like 'CS110'
 REM sectionNumber like '01'
+REM Up to 9 additional args can be provided following the required ones
+
 
 IF "%1"=="" GOTO usage
 
-IF "%4"=="" GOTO argError
+IF "%5"=="" GOTO argError
 
 SET filename=%1
 SET year=%2
@@ -49,6 +50,7 @@ IF "%8"=="" (
 
 IF "%port%"=="" SET port=5432
 
+
 REM SHIFT /1 moves parameters down by 1, making %1 out of scope, and allowing a new paramater to be referenced
 SHIFT /1
 SHIFT /1
@@ -59,13 +61,20 @@ SHIFT /1
 SHIFT /1
 SHIFT /1
 
-REM Empty parameters are extra and will be ignored
+
+REM Empty parameters will be ignored
 REM Using ^ allows a command to continue onto the next line
+REM psql's single-transaction flag results in all of the statements provided through the
+REM  -c and -f options to be run as a single transaction
+REM This allows the use of a temporary table without it being automatically dropped
+REM  due to the session being closed between statements
+
 psql %1 %2 %3 %4 %5 %6 %7 %8 %9 -h %hostname% -p %port% -d %database% -U %username%^
  --single-transaction -f "rosterImport.sql"^
  -c "\COPY rosterStaging FROM %filename% WITH csv HEADER"^
  -c "SELECT importFromRoster(%year%, '%season%', '%courseNumber%', '%sectionNumber%');"
 goto end
+
 
 :argError
 ECHO You must supply at least five arguments (filename year season courseNumber sectionNumber)
