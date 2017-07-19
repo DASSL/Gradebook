bea180cc3fb01c0a3bbfab85bde3494bd6ff5a21
Sean Murthy <murthys@wcsu.edu>
2017-07-18 21:46:10 -0400

    Fix issues with OpenClose import
    Fixes #24
    
    Moved all .sql files in this commit to the directory `/src/db`
    
    `createOpenCloseStagingTable.sql`
    - Added file
    
    `importOpenCloseCSV.bat`
    - Removed use of `ON_ERROR_STOP` because all commands are run in a single transaction
    
    `importOpenClose.sql`
    - Updated file prologue comment
    - Schema-qualified all table names
    - Changed `gradebook` to `Gradebook`
    - Reformatted to make line length ~80 characters (a few lines still need fixing)
    
    Tested the changes by importing Spring2017OpenClose.csv
    - I believe the results are the same as the ones before changes were made
    - Copied 1757 rows to the staging table
    - Created the following new rows
       - Terms: 1
       - Courses: 748
       - Instructors: 562
       - Sections: 1450

--- src/db/createOpenCloseStagingTable.sql
+++ src/db/createOpenCloseStagingTable.sql
@@ -1,17 +1,22 @@
---createOpenCloseStagingTable.sql - GradeBook
+--createOpenCloseStagingTable.sql - Gradebook
 
 --Kyle Bella, Zach Boylan, Zaid Bhujwala, Steven Rollo, Sean Murthy
 --Data Science & Systems Lab (DASSL), Western Connecticut State University (WCSU)
 
 --(C) 2017- DASSL. ALL RIGHTS RESERVED.
---Licensed to others under CC 4.0 BY-SA-NC: https://creativecommons.org/licenses/by-nc-sa/4.0/
+--Licensed to others under CC 4.0 BY-SA-NC
+--https://creativecommons.org/licenses/by-nc-sa/4.0/
 
 --PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
 
---Creating temporary table for openCloseStaging. Table will be used to copy from CSV files and later to be
---used to populate the database. 
+--This script creates a temporary staging table to import OpenClose database
+-- the table is used to stage data from CSV file as part of the import process
 
-CREATE TEMPORARY TABLE gradebook.openCloseStaging
+--This script should be the first to run when importing OpenClose data
+-- there should be no need to run this script separately, because it is
+-- incorporated into the import process batch file importOpenCloseCSV.bat
+
+CREATE TEMPORARY TABLE openCloseStaging
 (
    Status VARCHAR(6),
    Level VARCHAR(2),
--- src/db/importOpenCloseCSV.bat
+++ src/db/importOpenCloseCSV.bat
@@ -37,7 +37,7 @@ IF "%6"=="" (
 
 IF "%port%"=="" SET port=5432
 
-psql -h %hostname% -p %port% -d %database% -U %username% --single-transaction -c "\set ON_ERROR_STOP on" -c "\i createOpenCloseStagingTable.sql" -c "\COPY openCloseStaging FROM %1 WITH csv HEADER" -c "SELECT gradebook.importOpenClose(%2, '%3', false);"
+psql -h %hostname% -p %port% -d %database% -U %username% --single-transaction -c "\i createOpenCloseStagingTable.sql" -c "\COPY openCloseStaging FROM %1 WITH csv HEADER" -c "SELECT Gradebook.importOpenClose(%2, '%3', false);"
 goto end
 
 :argError
