c66a23aab81c3df4dc9f24b309e3e2790b8af5ed
Steven Rollo <srrollo@gmail.com>
2017-08-22 15:29:06 -0400

    Removed instructor_unnest function and replaced with a CTE
    checkTermSequence is only called once, and the result is stored in a variable
    The instructor name splitting and resaasembling has been cleaned up somewhat
    Usage of array code and unnest has been reduced
    A second CTE has been added to the Section insert procedure which UNIONS any existing instructors to those inserted and returned in the returning clause
    This fixes an issue where multiple terms could not be imported if there was any one instructor that taught in both terms

--- src/db/importOpenClose/prepareOpenCloseImport.sql
+++ src/db/importOpenClose/prepareOpenCloseImport.sql
@@ -13,8 +13,6 @@
 -- it creates some temporary objects needed for import
 -- it should be run before copying csv data to the staging table
 
---The script addSectionMgmt.sql should have been run before running this script
-
 --This table is used to stage data from CSV file as part of the import process
 CREATE TEMPORARY TABLE openCloseStaging
 (
@@ -39,21 +37,39 @@ CREATE TEMPORARY TABLE openCloseStaging
    Instructor VARCHAR(200)
 );
 
---A function to transform a comma-separated string of values to a table
--- each row in the result table will have one item from the CSV string
-CREATE FUNCTION pg_temp.instructorUnnest(instructorNames VARCHAR(200))
-RETURNS TABLE(Name VARCHAR(100)) AS
+--Checks if the supplied year and season belong to the next term in sequence.
+-- Returns true if the supplied term is the next term, otherwise false.
+-- This is accomplished using the expression:
+-- currentYear * COUNT(Seasons) + Season + 1 =
+-- newYear * COUNT(Seasons) + Season
+-- Essentially, each year is mapped to a scale counting for the number of
+-- seasons that are in Gradebook.  This allows a simple equality check to see
+-- if the supplied term is in sequence
+CREATE FUNCTION pg_temp.checkTermSequence(Year INT, Season VARCHAR(10))
+RETURNS BOOLEAN AS
 $$
-   SELECT
-   trim(
-      unnest
-      (
-         string_to_array
+   --Get each term from the latest year
+   WITH latestYear AS
+   (
+      SELECT Year, Season
+      FROM Gradebook.Term
+      WHERE Year = (SELECT MAX(Year) FROM Gradebook.Term)
+   )
+   SELECT CASE --Check for the case when there are no terms, as we can't check
+               --the sequence if it hasn't been started yet
+      WHEN (SELECT COUNT(*) FROM Gradebook.Term) > 0 THEN
          (
-            replace($1, '(P)', ''), ','
+            MAX(LY.Year) * (SELECT COUNT(*) FROM Gradebook.Season) +
+            MAX(LY.Season) + 1
+         ) =
+         (
+            $1 * (SELECT COUNT(*) FROM Gradebook.Season) +
+            (SELECT "Order" FROM Gradebook.Season WHERE name = $2 OR code = $2)
          )
-      )
-   ) "name"
+      ELSE
+         TRUE
+      END
+   FROM latestYear LY
 $$ LANGUAGE sql;
 
 --Populates Term, Instructor, Course, Course_Section and Section_Instructor from
@@ -65,11 +81,13 @@ CREATE FUNCTION pg_temp.importOpenClose(Year INT, Season VARCHAR(10),
                                        )
 RETURNS VOID AS
 $$
+DECLARE
+   termInSequence BOOLEAN;
 BEGIN
-   IF NOT (SELECT Gradebook.checkTermSequence($1, $2)) AND useSequence THEN
+   SELECT pg_temp.checkTermSequence($1, $2) INTO termInSequence;
+   IF NOT termInSequence AND useSequence THEN
       RAISE EXCEPTION 'Error - Supplied term is out of sequence';
-   ELSIF NOT (SELECT Gradebook.checkTermSequence($1, $2))
-         AND NOT useSequence THEN
+   ELSIF NOT (termInSequence OR useSequence) THEN
       RAISE NOTICE 'Sequence check failed, but will be overriden';
    END IF;
 
@@ -83,8 +101,9 @@ BEGIN
          --end date
          INSERT INTO Gradebook.Term(Year, Season, StartDate, EndDate)
          SELECT $1,
-         (SELECT "Order" FROM Gradebook.Season s
-         WHERE s.Name = $2 OR s.Code = $2
+         (
+            SELECT "Order" FROM Gradebook.Season s
+            WHERE s.Name = $2 OR s.Code = $2
          ),
          $1 + MIN(to_date(sDate, 'MM-DD')),
          $1 + MAX(to_date(eDate, 'MM-DD'))
@@ -99,25 +118,55 @@ BEGIN
    AND NOT course IS NULL
    ON CONFLICT DO NOTHING;
 
-   WITH instructorFullNames AS
+   --The first CTE inserts new instructors into Gradebook.Instructor, and RETURNS
+   -- their full names for insertion into  Gradebook.Section table
+   WITH insertedFullNames AS
    (
+      --This CTE creates a table of individual instructor names from single section,
+      -- created from the multi-name csv list provided in the openclose csv files
+      -- The names are then split into arrays using spaces as a delimiter, creating a
+      -- table of instructor name arrays
+      WITH instructorSplitNames AS
+      (
+         SELECT DISTINCT
+            regexp_split_to_array( --This split isolates FName, MName, and LName
+               trim(
+                  regexp_split_to_table(
+                     replace(instructor, '(P)', ''), ','
+                  )
+               ), ' '
+            ) "name"
+         FROM pg_temp.openCloseStaging
+         WHERE instructor LIKE '% %' --Right now we can't handle names that clearly
+                                     --are missing a first or last name
+                                     --also ignores "names" like 'TBA'
+                                     --LIKE '% %' checks for at least one space
+      )
       INSERT INTO Gradebook.Instructor (FName, MName, LName)
-      SELECT Name[1], CASE
-         WHEN array_length(Name, 1) < 3 THEN  NULL
-         ELSE (SELECT string_agg(n, ' ')
+      --Select the name parts from the array into the new Instructor row
+      --EX. Name[1] = FName
+      SELECT Name[1],
+         --If there are less than 3 enteries in the name array, we assume there is no MName
+         CASE WHEN array_length(Name, 1) < 3 THEN NULL
+         ELSE ( --Because some names have more than 3 'name parts', we concat all
+                --parts except the first and last into MName
+               SELECT string_agg(n, ' ')
                FROM unnest(Name[2:array_length(Name, 1) - 1]) n
-              )
-      END, Name[array_length(Name, 1)]
-      FROM (SELECT DISTINCT
-               string_to_array(Gradebook.instructorUnnest(instructor), ' ') "name"
-            FROM pg_temp.openCloseStaging
-            WHERE instructor LIKE '% %'--Right now we can't handle names that clearly
-                                         --are missing a first or last name
-                                         --also ignores "names" like 'TBA'
-                                         --LIKE '% %' checks for at least one space
-           ) instructorNames
-      RETURNING id,
-                FName || ' ' || COALESCE(MName || ' ', '') || LName as FullName
+              ) END,
+         Name[array_length(Name, 1)] --We place the last name part into LName
+      FROM instructorSplitNames
+      ON CONFLICT DO NOTHING
+      RETURNING id, FName || ' ' || COALESCE(MName || ' ', '') || LName as FullName
+   ),
+   --This second CTE UNIONS any existing instructors that were not inserted into
+   -- Gradebook.Instructor so they can be used for insertion into Gradebook.Section
+   instructorFullNames AS
+   (
+      SELECT id, FullName
+      FROM insertedFullNames
+      UNION
+      SELECT id, FName || ' ' || COALESCE(MName || ' ', '') || LName as FullName
+      FROM Gradebook.Instructor
    )
    INSERT INTO Gradebook.Section(CRN, Course, SectionNumber, Term, Schedule,
                                  StartDate, EndDate,
