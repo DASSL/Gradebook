9f04b273e1a4c9f232c5efcf618b1eca7f1bea58
Andrew Figueroa <figueroa039@connect.wcsu.edu>
2017-08-22 20:08:58 -0400

    Make add. improvements to Attendance import script
    Clarify header comments that describe order of running imports and
    location requirements of sample data files.
    
    Change import function to take sectionID, which allows the removal of
    CTE that found sectionID. SectionID is now found with a call to
    Gradebook.getSectionID before calling the import function.
    
    Add statement that INSERTs any missing attendance codes from the sample
    data into AttendanceStatus.
    
    Revert removal of WHERE a.Code IS NOT NULL from last commit. Add comment
    describing behavior when NULL is encountered in staging table.
    
    Change table alias of Student table now that there is no ambiguity with
    SectionID. (Was 'stu', now 's')
    
    Change case of function language to match other scripts
    ('LANGUAGE SQL' -> 'LANGUAGE sql').

--- tests/data/Attendance/importAttendance.sql
+++ tests/data/Attendance/importAttendance.sql
@@ -10,19 +10,21 @@
 --PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
 
 
---Due to the use of the /COPY meta-command, this script needs to be run using the
+--Due to the use of the \COPY meta-command, this script needs to be run using the
 -- psql command line tool provided with most PostgreSQL installations. The current 
 -- working directory needs be the same as this script's location.
 
---This script should be run after importing the Roster test data, but before any
--- humanization of student names occurs.
+--This script should only be run after importing all sample Roster data provided
+-- in \tests\data\Roster, as there is a correspondance between the sample Roster
+-- and sample Attendance data.
+--Since the names in the sample data must match those in the Student table,
+-- this script should be run before any humanization of student names occurs.
 
---The following three files contain sample Attendance data that corresponds with
--- the sample Roster data provided in \tests\data\Roster. The files must be in 
--- the same directory as this script:
--- 2017SpringCS110-05Attendance.csv
--- 2017SpringCS110-72Attendance.csv
--- 2017SpringCS110-74Attendance.csv
+--The following files, which contain the sample Attendance data, must be in the
+-- same directory as this script:
+--  2017SpringCS110-05Attendance.csv
+--  2017SpringCS110-72Attendance.csv
+--  2017SpringCS110-74Attendance.csv
 
 
 --Populate AttendanceStatus with necessary attendance codes
@@ -47,37 +49,41 @@ CREATE TABLE pg_temp.AttendanceStaging
 
 
 --Define a temporary function for moving data from staging table to AttendanceRecord
-CREATE OR REPLACE FUNCTION pg_temp.importAttendance(
-   Year NUMERIC(4,0), Season NUMERIC(1,0), Course VARCHAR(8), 
-   SectionNumber VARCHAR(3)) 
+CREATE OR REPLACE FUNCTION pg_temp.importAttendance(sectionID INT)
    RETURNS VOID AS
 $$
+   --Add AttendanceStaging.Code values that do not exist in AttendanceStatus.
+   -- Code is also used as both the Status and Description attributes if an INSERT
+   -- is performed. Does not ignore conflicts on AttendanceStatus.Description. NULL
+   -- Code values are interpreted as 'P'.
+   INSERT INTO Gradebook.AttendanceStatus
+   SELECT DISTINCT COALESCE(UPPER(Code), 'P'), COALESCE(UPPER(Code), 'P')
+   FROM pg_temp.AttendanceStaging
+   ON CONFLICT (Status) DO NOTHING;
+   
    --Match student from each entry in sample data with their corresponsing entry in 
-   -- Student table by joining on a match of the 3 name parts. MName can be NULL.
+   -- the Student table by joining on a match of the 3 name parts. MName can be NULL
+   -- and rows with NULL as the attendance code are interpreted as Present and not
+   -- imported. (See behavior of Gradebook.getAttendance() in src/db/getAttendance.sql)
    INSERT INTO Gradebook.AttendanceRecord
-   WITH SectionID AS
-   (
-      SELECT s.ID
-      FROM Gradebook.Section s JOIN Gradebook.Term t ON s.Term = t.ID AND t.Year = $1
-       AND t.Season = $2 AND s.Course = $3 AND s.SectionNumber = $4
-   )
-   SELECT stu.ID, sectionID.ID, a.Date, a.Code
-   FROM SectionID, pg_temp.AttendanceStaging a JOIN Gradebook.Student stu ON
-        a.FName = stu.FName AND a.LName = stu.LName AND
-        COALESCE(a.MName, '') = COALESCE(stu.MName, '')
+   SELECT s.ID, $1, a.Date, a.Code
+   FROM pg_temp.AttendanceStaging a JOIN Gradebook.Student s ON
+        a.FName = s.FName AND a.LName = s.LName AND
+        COALESCE(a.MName, '') = COALESCE(s.MName, '')
+   WHERE a.Code IS NOT NULL
    ON CONFLICT DO NOTHING;
-$$ LANGUAGE SQL;
+$$ LANGUAGE sql;
 
 
 --Import data from files to staging table and call import function for each section
 \COPY pg_temp.AttendanceStaging FROM '2017SpringCS110-05Attendance.csv' WITH csv HEADER
-SELECT pg_temp.importAttendance(2017, 0, 'CS110', '05');
+SELECT pg_temp.importAttendance(Gradebook.getSectionID(2017, '0', 'CS110', '05'));
 TRUNCATE pg_temp.AttendanceStaging;
 
 \COPY pg_temp.AttendanceStaging FROM '2017SpringCS110-72Attendance.csv' WITH csv HEADER
-SELECT pg_temp.importAttendance(2017, 0, 'CS110', '72');
+SELECT pg_temp.importAttendance(Gradebook.getSectionID(2017, '0', 'CS110', '72'));
 TRUNCATE pg_temp.AttendanceStaging;
 
 \COPY pg_temp.AttendanceStaging FROM '2017SpringCS110-74Attendance.csv' WITH csv HEADER
-SELECT pg_temp.importAttendance(2017, 0, 'CS110', '74');
+SELECT pg_temp.importAttendance(Gradebook.getSectionID(2017, '0', 'CS110', '74'));
 TRUNCATE pg_temp.AttendanceStaging;
